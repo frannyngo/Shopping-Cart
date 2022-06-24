8b8b52d869984f5314bb60daadc4516e787d4f32
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-23 22:50:13 -0700

    nav links

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+import { Section } from "../../pages/GlobalStyles";
+
 export default function NavBar() {
-  return <p>Nav</p>;
+  return (
+    <Section color="white">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/store" style={{ textDecoration: "none" }}>
+        Store
+      </Link>
+      <Link to="/about" style={{ textDecoration: "none" }}>
+        About
+      </Link>
+    </Section>
+  );
 }
