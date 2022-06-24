8c9ec710320dbba88d37c96fc0550eb4dfc900f7
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-23 22:16:55 -0700

    packages, routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
       <p>hello world</p>
     </div>
   );
