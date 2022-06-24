9e012e60bd3ed3c5a18f2f93e982d2c986056c89
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-23 22:28:48 -0700

    styled components and nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Store from "./pages/Store";
+import NavBar from "./components/Nav/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
