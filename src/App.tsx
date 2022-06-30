ca081dadba68dcd49475d7e3cc61f80b99442c34
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-06-29 23:18:06 -0700

    userContext shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import About from "./pages/About";
 import Store from "./pages/store/Store";
 import NavBar from "./components/Nav/NavBar";
 import { Theme } from "./pages/GlobalStyles";
+import { ShoppingCartProvider } from "../src/functions/shoppingCart";
 
 function App() {
   return (
-    <Theme>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-      <p>hello world</p>
-    </Theme>
+    <ShoppingCartProvider>
+      <Theme>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+        <p>hello world</p>
+      </Theme>
+    </ShoppingCartProvider>
   );
 }
 
