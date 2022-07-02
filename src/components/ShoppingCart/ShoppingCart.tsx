91cf4ad5b91e87e10b41c57a0d583bf1c2e7724d
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-07-01 23:46:47 -0700

    shopping cart component

---
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,9 @@
+import { ShoppingCartContainer } from "./ShoppingCartStyles";
+
+export default function ShoppingCart() {
+  return (
+    <ShoppingCartContainer>
+      <p>hi</p>
+    </ShoppingCartContainer>
+  );
+}
---
+++ src/components/ShoppingCart/ShoppingCartStyles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ShoppingCartContainer = styled.div`
+  background: pink;
+  width: 100%;
+  min-height: 200px;
+`;
