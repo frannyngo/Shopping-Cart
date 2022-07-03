cae9bc1ddc1945342bdff27573596b07c1218bab
frannyngo <85131610+frannyngo@users.noreply.github.com>
2022-07-02 21:21:48 -0700

    remove from cart functionality

--- src/components/CartItems/CartItems.tsx
+++ src/components/CartItems/CartItems.tsx
@@ -1,6 +1,13 @@
 import { useShoppingCart } from "../../functions/shoppingCart";
-import { CartItemContainer } from "./CartItemsStyles";
+import {
+  CartItemContainer,
+  Details,
+  NameContainer,
+  TotalContainer,
+} from "./CartItemsStyles";
 import storeItems from "../../constants/items.json";
+import formatCurrency from "../../functions/formatCurrency";
+import { CloseButton } from "../ShoppingCart/ShoppingCartStyles";
 
 type CartItemsProps = {
   id: number;
@@ -11,12 +18,26 @@ export default function CartItems({ id, quantity }: CartItemsProps) {
   const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((item) => item.id === id);
   if (item === null) return null;
+
   return (
     <CartItemContainer>
-      <img src={item?.imgUrl} style={{ height: 125, width: 75 }} />
-      <p>{item?.name}</p>
-      <p>{item?.price}</p>
-      <p>{quantity}</p>
+      <img src={item?.imgUrl} style={{ height: 125, width: 125 }} />
+      <Details>
+        <NameContainer>
+          <p style={{ fontSize: 20 }}>{item?.name}</p>
+          <span style={{ color: "grey", marginLeft: 10 }}>x{quantity}</span>
+        </NameContainer>
+        <p style={{ fontSize: 20, color: "grey" }}>${item?.price}</p>
+      </Details>
+      <TotalContainer>
+        {formatCurrency(item!.price * quantity)}
+        <CloseButton
+          style={{ marginLeft: 10 }}
+          onClick={() => removeFromCart(item!.id)}
+        >
+          X
+        </CloseButton>{" "}
+      </TotalContainer>
     </CartItemContainer>
   );
 }
--- src/components/CartItems/CartItemsStyles.tsx
+++ src/components/CartItems/CartItemsStyles.tsx
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const CartItemContainer = styled.div`
-  background: pink;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Details = styled.div`
+  width: 100px;
+  margin-left: 15px;
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+`;
+
+export const TotalContainer = styled.div`
+  background: orange;
+  align-items: center;
+  justify-content: center;
+  display: flex;
 `;
